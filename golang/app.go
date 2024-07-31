c393f8e9e271ffdb6a37e3563860d89b34a00370
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-07-31 15:10:24 +0900

    fix: limit 20

--- golang/app.go
+++ golang/app.go
@@ -386,7 +386,13 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
+	err := db.Select(&results,
+		`SELECT p.id AS post_id, p.user_id AS user_id, p.body , p.mime, p.created_at
+		FROM posts as p FORCE INDEX (posts_created_at_idx) 
+			JOIN users as u ON p.user_id = u.id
+		WHERE u.del_flg = 0
+		ORDER BY p.created_at DESC LIMIT ?`,
+		postsPerPage)
 	if err != nil {
 		log.Print(err)
 		return
