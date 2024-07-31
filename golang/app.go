55c2475dc8fad2fcd3d38461c8e29ceeb7816dd3
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-07-31 17:28:34 +0900

    test

--- golang/app.go
+++ golang/app.go
@@ -949,11 +949,12 @@ func postIndex(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	query := "INSERT INTO `posts` (`user_id`, `mime`, `body`) VALUES (?,?,?)"
+	query := "INSERT INTO `posts` (`user_id`, `mime`, `imgdata`, `body`) VALUES (?,?,?,?)"
 	result, err := db.Exec(
 		query,
 		me.ID,
 		mime,
+		0,
 		r.FormValue("body"),
 	)
 	if err != nil {
